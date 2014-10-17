7a195e4fefa2402984cff2342b404f8d8780c353
Sasha Cooper <jinksy@gmail.com>
2014-10-17 16:38:59 +0100

    deals with 1 empty array case

--- lib/merge_sort.rb
+++ lib/merge_sort.rb
@@ -1,7 +1,26 @@
 class Array
 
 	def merge_sort
-		self
+		return self if self.length < 2
+
 	end
+
+	def merge(arr1, arr2)
+		first_of_first = arr1.first
+		first_of_second = arr2.first
+		if !first_of_first
+			self.push first_of_second
+		elsif !first_of_second
+			self.push first_of_first
+		elsif first_of_first < first_of_second
+			self.push first_of_first
+			self.push first_of_second
+		else
+			self.push first_of_second
+			self.push first_of_first
+		end
+	end
+
+	# def merge_terminal(arr)
 
 end
--- spec/merge_sort_spec.rb
+++ spec/merge_sort_spec.rb
@@ -16,4 +16,34 @@ describe 'merge sort' do
 		expect([1].merge_sort).to eq [1]
 	end
 
+	xit 'returns a sorted array from a 2-element array' do
+		expect([2,1].merge_sort).to eq [1,2]
+	end
+
+	describe 'merge' do
+
+		it 'given an empty array and a single-element array appends the element to self' do
+			expect([1].merge([], [2])).to eq [1, 2]
+		end
+
+		it 'given [2] and [3] appends first 2 then 3 to self' do
+			arr1 = [1]
+			arr2 = [2]
+			arr3 = [3]
+			expect(arr1).to receive(:push).with(2).ordered
+			expect(arr1).to receive(:push).with(3).ordered
+			arr1.merge(arr2, arr3)
+		end
+
+		it 'given [3] and [2] appends first 2 then 3 to self' do
+			arr1 = [1]
+			arr2 = [2]
+			arr3 = [3]
+			expect(arr1).to receive(:push).with(2).ordered
+			expect(arr1).to receive(:push).with(3).ordered
+			arr1.merge(arr3, arr2)
+		end
+
+	end
+
 end
